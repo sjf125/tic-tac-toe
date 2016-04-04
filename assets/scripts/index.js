a86027979e5019230f5712ac8f2ad57db153a025
sjf125 <sjf125@gmail.com>
2016-04-04 19:19:38 -0400

    Uncomment index.js and state.js
    Problem is with checks.js - un-commenting it breaks page.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,33 +1,33 @@
-// 'use strict';
-//
-//
-// const state = require('./state.js');
-//
-// const check = require('./checks.js');
-//
-// const resetGame = function() {
-//   state.turn = false;
-//   state.xWin = false;
-//   state.oWin = false;
-//   state.draw = false;
-//   for (let i = 0; i < state.board.length; i++) {
-//     state.board[i] = '';
-//   }
-// };
-//
-// const newGame = function() {
-//   $('.mark').one('click', function() {
-//     if ($(this).children().text() === '') {
-//       state.turn ? $(this).text(state.xToken) : $(this).text(state.oToken);
-//       check.checkBoard();
-//       check.checkWin();
-//       state.turn = !state.turn;
-//       ++state.moves;
-//     }
-//   });
-// };
-//
-// module.exports = {
-//   resetGame,
-//   newGame,
-// };
+'use strict';
+
+
+const state = require('./state.js');
+
+const check = require('./checks.js');
+
+const resetGame = function() {
+  state.turn = false;
+  state.xWin = false;
+  state.oWin = false;
+  state.draw = false;
+  for (let i = 0; i < state.board.length; i++) {
+    state.board[i] = '';
+  }
+};
+
+const newGame = function() {
+  $('.mark').one('click', function() {
+    if ($(this).children().text() === '') {
+      state.turn ? $(this).text(state.xToken) : $(this).text(state.oToken);
+      check.checkBoard();
+      check.checkWin();
+      state.turn = !state.turn;
+      ++state.moves;
+    }
+  });
+};
+
+module.exports = {
+  resetGame,
+  newGame,
+};
