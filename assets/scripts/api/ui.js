3768b73c6d898c680ab7b04496f7c233af9da1bf
sjf125 <sjf125@gmail.com>
2016-04-06 10:41:50 -0400

    Add show/hide button functionality
    Based on login state

--- assets/scripts/api/ui.js
+++ assets/scripts/api/ui.js
@@ -5,11 +5,21 @@ const app = require('./apiurl.js');
 const signInSuccess = (data) => {
   app.user = data.user;
   console.log(app);
+  $( document ).ready(function() {
+    $('#sign-in, #sign-up-btn').hide();
+    $('#sign-in-success').show().fadeOut(1000);
+    $('#sign-out, #change-pass-btn').delay(1000).show();
+  });
+
 };
 
 const signOutSuccess = () => {
   app.user = null;
   console.log(app);
+  $( document ).ready(function() {
+    $('#sign-out, #change-pass-btn').fadeOut(500);
+    $('#sign-in').delay(500).fadeIn(500);
+  });
 };
 
 const success = (data) => {
